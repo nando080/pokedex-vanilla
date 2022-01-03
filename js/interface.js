1d83cd87d8a1572567371d2a0db1956ec660e54a
nando080 <nando080@hotmail.com>
2022-01-03 12:19:51 -0300

    filter section activation js #1

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,7 @@
 const searchButtonEl = document.querySelector('[data-js="search-button"]')
+const filterContainerEl = document.querySelector('[data-js="filter"]')
 
 searchButtonEl.addEventListener('click', () => {
     searchButtonEl.classList.toggle('is-active')
+    filterContainerEl.classList.toggle('is-active')
 })
