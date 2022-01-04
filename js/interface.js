409edf1647b5186c6d54ff06daa6fb2dc86ce1c4
nando080 <nando080@hotmail.com>
2022-01-04 20:29:54 -0300

    filter section activation

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,30 @@
 const searchButtonEl = document.querySelector('[data-js="search-button"]')
-const filterContainerEl = document.querySelector('[data-js="filter"]')
+const filterSectionEl = document.querySelector('[data-js="filter"]')
+const filterContainerEl = filterSectionEl.querySelector('.filter__container')
+
+let isFilterActive = false
+
+const getFilterContanerHeight = () => filterContainerEl.getBoundingClientRect().height
+
+const setFilterActivation = () => {
+    const isFilterContainerActive = filterContainerEl.classList.contains('is-active')
+    console.log(isFilterContainerActive)
+    if (!isFilterActive && !isFilterContainerActive) {
+        filterSectionEl.style.height = `${getFilterContanerHeight()}px`
+        filterContainerEl.classList.add('is-active')
+        isFilterActive = true
+    } else {
+        filterSectionEl.style.height = '0px'
+        filterContainerEl.classList.remove('is-active')
+        isFilterActive = false
+    }
+}
 
 searchButtonEl.addEventListener('click', () => {
     searchButtonEl.classList.toggle('is-active')
-    filterContainerEl.classList.toggle('is-active')
+    setFilterActivation()
+})
+
+window.addEventListener('resize', () => {
+    setFilterActivation()
 })
