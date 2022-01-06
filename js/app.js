0cd6527fb8c1f965c6c45731ac2c4d7b71f18978
nando080 <nando080@hotmail.com>
2022-01-06 07:14:19 -0300

    fetch pokemons and insert into dom #1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const searchButtonEl = document.querySelector('[data-js="search-button"]')
 const filterSectionEl = document.querySelector('[data-js="filter"]')
 const filterContainerEl = filterSectionEl.querySelector('.filter__container')
+const pokemonsContainerEl = document.querySelector('.pokemon')
+
+const pokemons = []
 
 let isFilterActive = false
 
@@ -20,6 +23,60 @@ const setFilterActivation = () => {
     }
 }
 
+const getFormatedID = value => {
+    let id = ''
+    if (value < 10) {
+        id = `00${value}`
+        return id
+    }
+    if (value < 100) {
+        id = `0${value}`
+        return id
+    }
+    return value
+}
+
+const createPokemonElement = ({id, name, type}) => {
+    const pokeDiv = document.createElement('div')
+    pokeDiv.classList.add('pokemon__item')
+    pokeDiv.classList.add(`u-${type[0]}-bg`)
+    console.log(name)
+    pokeDiv.innerHTML = `
+    <img src="img/pokemon-gif/${name.toLowerCase()}.gif" alt="${name}" class="pokemon__img">
+    <p class="pokemon__id">${getFormatedID(id)}</p>
+    <h2 class="pokemon__name">${name.toUpperCase()}</h2>
+    `
+    return pokeDiv
+}
+
+const createAllPokemons = pokemonList => {
+    const allPokemons = []
+    pokemonList.forEach(pokemon => {
+        allPokemons.push(createPokemonElement(pokemon))
+    })
+    return allPokemons
+}
+
+const insertPokemonsIntoDOM = pokemonElements => {
+    pokemonElements.forEach(pokemonEl => {
+        pokemonsContainerEl.appendChild(pokemonEl)
+    })
+}
+
+const fillPokemonsList = pokemonList => {
+    pokemonList.forEach(pokemon => {
+        pokemons.push(pokemon)
+    })
+}
+
+const fetchPokemons = async () => {
+    const data = await fetch('../db/pokedex.json')
+    const pokelist = await data.json()
+    fillPokemonsList(pokelist)
+    insertPokemonsIntoDOM(createAllPokemons(pokelist))
+    console.log(pokemons);
+}
+
 searchButtonEl.addEventListener('click', () => {
     searchButtonEl.classList.toggle('is-active')
     setFilterActivation()
@@ -28,3 +85,5 @@ searchButtonEl.addEventListener('click', () => {
 window.addEventListener('resize', () => {
     setFilterActivation()
 })
+
+fetchPokemons()
